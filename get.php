fbc3dd2e6cff8a0bf1f1aaab98dd8dbd96c896d7
Dark <aprilgowk@gmail.com>
2015-05-12 02:01:08 +0300

    增加了txt文本写入功能

--- get.php
+++ get.php
@@ -1,9 +1,25 @@
 <html>
-<head><title>Register PHP</title></head>
+<head><title>提交成功！</title></head>
 <body>
+
 <?php 
 $name = $_POST['name'];
+$birth = $_POST['birth'];
+$sex = $_POST['sex'];
+$contact_person = $_POST['contactN'];
+$telephone = $_POST['telephone'];
+$recommended = $_POST['recommended'];
+$email = $_POST['email'];
+$experience = $_POST['ex'];
 
+$content = "name: ".$name.";birth: ".$birth.";sex: ".$sex.";contact_person: ".$contact_person.";telephone: ".$telephone.";recommended: ".$recommended.";email: ".$email.";experience: ".$experience."\n\n";
+			
 
+$file = fopen("record.txt","a+");
+fwrite($file,$content);
+fclose($file);
+?>
+
+<h1>提交成功！您可以关掉这个界面。</h1>
 </body>
 </html>
