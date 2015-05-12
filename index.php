ae93de2ff7bcc9effee0252bcf01002707b99586
Dark <aprilgowk@gmail.com>
2015-05-12 17:30:28 +0300

    防止重复输入表单

--- index.php
+++ index.php
@@ -11,56 +11,67 @@
     <img src="img/logo.jpg" alt="logo" class="logo">
   </header>
 
-<?php
-//测试输入是否为空或正确格式
+  <?php
+  //测试输入是否为空或正确格式
   $nameErr = $emailErr = $sexErr = $birthErr = $contactErr = $telErr = $exErr = "";
+  
+  $all_pass = 8; //记录是否都通过的变量
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $all_pass = 7; 
    if (empty($_POST["name"])) {
      $nameErr = "姓名是必填的";
+     $all_pass -= 1;
    } else {
      $name = test_input($_POST["name"]);
    }
    
    if (empty($_POST["email"])) {
      $emailErr = "电邮是必填的";
+     $all_pass -= 1;
    } else {
      $email = test_input($_POST["email"]);
      // 检查电子邮件地址语法是否有效
      if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) {
        $emailErr = "无效的 电邮/email 格式"; 
+       $all_pass -= 1;
      }
    }
 
    if (empty($_POST["birth"])) {
      $birthErr = "生日是必填的";
+     $all_pass -= 1;
    } else {
      $birth = test_input($_POST["birth"]);
    }
 
    if (empty($_POST["contactN"])) {
      $contactErr = "联系人是必填的";
+     $all_pass -= 1;
    } else {
      $contact_person = test_input($_POST["contact_person"]);
    }
 
    if (empty($_POST["sex"])) {
      $sexErr = "性别是必选的";
+     $all_pass -= 1;
    } else {
      $sex = test_input($_POST["sex"]);
    }
 
    if (empty($_POST["telephone"])) {
      $telErr = "电话是必填的";
+     $all_pass -= 1;
    } else {
      $telephone = test_input($_POST["telephone"]);
    }
 
    if (empty($_POST["ex"])) {
      $exErr = "经验是必选的";
+     $all_pass -= 1;
    } else {
      $ex = test_input($_POST["ex"]);
-   }   
+   }
  }
 
  function test_input($data) {
@@ -78,55 +89,65 @@
     芬兰赛区
   </p>
   <section>
-  <ul id="gallery">
-    <li class="slogan">
-      <p class="s1">放飞梦想</p>
-      <p>唱响中国</p>
-    </li>
-    <li>
-      <form method="post" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
-        <p><span id="special">* 必需填写的部分 Where is required</span></p>
-        <span class="error">* <?php echo $nameErr;?></span>
-        姓名  /  Your name
-        <br>
-        <input type="text" name="name">
-        <br><br>
-        <span class="error">* <?php echo $birthErr;?></span>
-        出生年月  /  Day Of Birth (填写格式：日月年 ｜ dd.mm.yyyy)
-        <br>
-        <input type="text" name="birth">
-        <br><br>
+    <ul id="gallery">
+      <li class="slogan">
+        <p class="s1">放飞梦想</p>
+        <p>唱响中国</p>
+      </li>
+      <li>
+        <form method="post" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
+          <p><span id="special">* 必需填写的部分 Where is required</span></p>
+          <span class="error">* <?php echo $nameErr;?></span>
+          姓名  /  Your name
+          <br>
+          <input type="text" name="name">
+          <br><br>
+          <span class="error">* <?php echo $birthErr;?></span>
+          出生年月  /  Day Of Birth (填写格式：日.月.年 ｜ dd.mm.yyyy)
+          <br>
+          <input type="text" name="birth">
+          <br><br>
 
-        <span class="error">* <?php echo $sexErr;?></span>
-        性别  /  Gender<br>
-        <input type="radio" name="sex" value="male">男 / Male
-        <input type="radio" name="sex" value="female">女 / Female
-        <br><br>
-        <span class="error">* <?php echo $contactErr;?></span>
-        联系人姓名  /  Contact person
-        <br>
-        <input type="text" name="contactN">
-        <br><br>
-        <span class="error">* <?php echo $telErr;?></span>
-        联系人电话  /  Telephone<br>
-        <input type="text" name="telephone">
-        <br><br>
-        推荐单位  /  Recommended<br>
-        <input type="text" name="recommended">
-        <br><br>
-        <span class="error">* <?php echo $emailErr;?></span>
-        电邮地址  /  E-mail:<br>
-        <input type="text" name="email">
-        <br><br>
-        <span class="error">* <?php echo $exErr;?></span>
-        曾经在舞台上表演过  /  Experience performance<br>
-        <input type="radio" name="ex" value="yes">是 / Yes
-        <input type="radio" name="ex" value="no">否 / No
-        <br><br>
-        <input type="submit" name="submit" value="提交/Submit">
-      </form>
-    </li>
-  </ul>
+          <span class="error">* <?php echo $sexErr;?></span>
+          性别  /  Gender<br>
+          <input type="radio" name="sex" value="male">男 / Male
+          <input type="radio" name="sex" value="female">女 / Female
+          <br><br>
+          <span class="error">* <?php echo $contactErr;?></span>
+          联系人姓名  /  Contact person
+          <br>
+          <input type="text" name="contactN">
+          <br><br>
+          <span class="error">* <?php echo $telErr;?></span>
+          联系人电话  /  Telephone<br>
+          <input type="text" name="telephone">
+          <br><br>
+          推荐单位  /  Recommended<br>
+          <input type="text" name="recommended">
+          <br><br>
+          <span class="error">* <?php echo $emailErr;?></span>
+          电邮地址  /  E-mail:<br>
+          <input type="text" name="email">
+          <br><br>
+          <span class="error">* <?php echo $exErr;?></span>
+          曾经在舞台上表演过  /  Experience performance<br>
+          <input type="radio" name="ex" value="yes">是 / Yes
+          <input type="radio" name="ex" value="no">否 / No
+          <br><br>
+          <input type="submit" name="submit" value="提交/Submit" >
+
+          <!-- 如果所有输入结果正确，则执行record -->
+          <?php if($all_pass < 7){
+                    echo "<script type='text/javascript'>alert('Error:Need to fill again! 有地方填写错误，请按照提示重新填写。');</script>";
+                  }
+                if($all_pass === 7){
+                    include 'record.php';
+                  }
+          ?>
+
+        </form>
+      </li>
+    </ul>
   </section>
 </div>
 <!-- End .primary-content -->
---
+++ record.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<link rel="stylesheet" href="css/php.css">
+	<title>提交成功！</title>
+</head>
+<body>
+
+	<?php 
+	$name = $_POST['name'];
+	$birth = $_POST['birth'];
+	$sex = $_POST['sex'];
+	$contact_person = $_POST['contactN'];
+	$telephone = $_POST['telephone'];
+	$recommended = $_POST['recommended'];
+	$email = $_POST['email'];
+	$experience = $_POST['ex'];
+
+//把获取到的资料写入到本地csv格式的文件中
+	$list = array
+	(
+		$content = "".$name.",".$birth.",".$sex.",".$contact_person.",".$telephone.",".$recommended.",".$email.",".$experience,
+		);
+	$file = fopen("record.csv","a+");
+	foreach ($list as $line)
+	{
+		fputcsv($file,split(',',$line));
+	}
+	fclose($file);
+	?>
+	<script type='text/javascript'>alert('恭喜您报名成功！Register completed!');</script>
+
+</body>
+</html>
